935212ed1af92e95e4f7440546524aa30fe86327
Glenn Chiang <glennchiang24@gmail.com>
2023-07-20 21:40:02 +0800

    Fixed timer bug

--- src/views/Play/Timer.tsx
+++ src/views/Play/Timer.tsx
@@ -1,15 +1,22 @@
 import { faClock } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 type props = {
   timeLimit: number;
   gameActive: boolean;
   endGame: () => void;
-}
+};
 
-export default function Timer({timeLimit, gameActive, endGame}: props) {
+export default function Timer({ timeLimit, gameActive, endGame }: props) {
   const [timeLeft, setTimeLeft] = useState(timeLimit);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft((timeLeft) => timeLeft - 1);
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
 
   useEffect(() => {
     if (!gameActive) {
@@ -17,21 +24,15 @@ export default function Timer({timeLimit, gameActive, endGame}: props) {
       return;
     }
 
-    const timer = setInterval(() => {
-      setTimeLeft(timeLeft => timeLeft - 1);
-    }, 1000);
-
     if (timeLeft === 0) {
       endGame();
     }
-
-    return () => clearInterval(timer);
-  }, [gameActive, timeLimit, timeLeft, endGame]);
+  }, [timeLeft, gameActive, timeLimit, endGame]);
 
   return (
     <div className="flex items-center gap-3 p-4 text-2xl">
-      <FontAwesomeIcon icon={faClock}/>
+      <FontAwesomeIcon icon={faClock} />
       {timeLeft}
     </div>
-  )
+  );
 }
